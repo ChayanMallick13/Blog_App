4892d914c32bde7772528de7fbe3d77784644d5d
Chayan_Mallick_01 <141207003+ChayanMallick13@users.noreply.github.com>
2025-01-08 22:23:59 +0530

    "backend commit1"

---
+++ Backend/index.js
@@ -0,0 +1,25 @@
+const version = '0.1';
+
+const express = require('express');
+const app = express();
+
+if(!process.env.NODE_ENV){
+    require('dotenv').config();
+}
+
+app.use(express.json());
+
+const router = require('./Routes/Routes');
+
+app.use(`/api/v${version}`,router);
+
+const dbConnect = require('./Config/dbConnect');
+
+dbConnect();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT,() => {
+    console.log('Server Started at PORT '+PORT);
+});
+
