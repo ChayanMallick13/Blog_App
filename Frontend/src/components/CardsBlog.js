f41c1a1e47ef928f487cab59aa796793936f18c9
Chayan_Mallick_01 <141207003+ChayanMallick13@users.noreply.github.com>
2025-01-09 20:24:38 +0530

    card chnaged

--- Frontend/src/components/CardsBlog.js
+++ Frontend/src/components/CardsBlog.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from 'react';
+import border from '../border1.png';
 
 const CardsBlog = ({ blog }) => {
     console.log(blog);
     return (
-        <div className='flex flex-col gap-y-14 bg-white px-4 py-4'>
-            <p >{blog.title}</p>
+        <div className='flex flex-col gap-y-12 bg-white px-4 py-4 w-[380px] text-center rounded-md shadow-black
+        shadow-lg hover:scale-110 drop-shadow-lg card-back relative transition-all duration-500 group border-slate-700
+        border-2
+        '>
+            <img alt='border' loading='lazy' src={border} className='absolute h-[90%] opacity-0 left-0 top-0
+            group-hover:opacity-10 transition-all duration-500
+            '/>
+            <p className='font-bold text-2xl'>{blog.title}</p>
             <p>{blog.body}</p>
-            <p>Category- {blog.category}</p>
-            <div>
+            <p className='font-medium cursor-pointer'>Category- {blog.category}</p>
+            <div className='w-full flex justify-between px-1 italic'>
                 <p> By- {blog.user}</p>
                 <p>username- {blog.username}</p>
             </div>
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -10,16 +10,16 @@ const Home = () => {
     const {loadingB , loadingU , blogs , users} = useContext(AppContext) ; 
 
     return (
-        <div className=' w-full flex flex-col '>
+        <div className=' w-[96%] flex flex-col mx-auto mt-[2rem]'>
             
-            <h1 className=' text-red-800'>Latest</h1>
+            <h1 className=' text-red-800 text-4xl mb-10 font-extrabold'>Latest:</h1>
             {
                 loadingB ? (
                     <Spinner/>
                 ) : 
                 (
                     blogs.length === 0 ? (<div>No post Available</div>) : 
-                    <div className='flex gap-5'>
+                    <div className='flex gap-10'>
                     {
 
                     (blogs.map((blog) => (
@@ -42,7 +42,7 @@ const Home = () => {
                 ) : 
                 (
                     users.length === 0 ? (<div>No user found</div>) : 
-                    <div className='flex gap-10'>
+                    <div className='flex gap-x-10'>
                         {
                             (users.map((user) => (
                                 <CardUser key= {user._id} user = {user} /> 
