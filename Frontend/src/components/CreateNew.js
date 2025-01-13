dd88aeb297f4c2d3a2af1e0403286302f358cfab
aditydev000 <adityadev1222004@gmail.com>
2025-01-14 00:57:20 +0530

    Update CreateNew.js

--- Frontend/src/components/CreateNew.js
+++ Frontend/src/components/CreateNew.js
@@ -17,7 +17,10 @@ const CreateNew = () => {
 
     <div className='flex gap-5 items-center group'>
 
-      <p className='hidden group-hover:flex font-bold transition-all duration-300 text-xl' >Create New Blog</p>
+      <div className='hidden group-hover:flex transition-all duration-300'>
+
+      <p className='   font-bold p-3 px-4 rounded-md  bg-slate-700   text-xl text-white' >Create New Blog</p>
+      </div>
       <button onClick={clickHandler} className="flex items-center justify-center w-14 h-14 bg-slate-600 text-white rounded-full hover:bg-slate-800 focus:outline-none focus:ring-2 focus:ring-slate-300  ">
           <span className="text-xl font-bold">+</span>
         </button>
